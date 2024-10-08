1f48f21f3528dcfe878fa54226831de0bc86bf60
neko __pathatha <ccarveg3010@g.educaand.es>
2024-10-08 13:37:38 +0200

    a

---
+++ Programación/UD1/src/EJ/EJ1.java
@@ -0,0 +1,15 @@
+package EJ;
+
+import java.util.Scanner;
+
+public class EJ1 {
+
+	public static void main(String[] args) {
+		// TODO cosas basicas que se hacen en la ud 1
+		Scanner scan=new Scanner(System.in);
+		System.out.println("hola");
+		String nom = scan.nextLine();
+		scan.close();
+	}
+
+}
